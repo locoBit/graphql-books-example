96b0a3e346b335c194c2f608e707f8749c333491
Alan Hernández <alan@agavelab.com>
2019-04-25 00:41:39 -0500

    Modularizing resolvers

---
+++ index.js
@@ -0,0 +1,25 @@
+const { ApolloServer, gql } = require('apollo-server');
+const resolvers = require('./app/resolvers');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Author {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [ Book ]
+    authors: [ Author ]
+  }
+`;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
